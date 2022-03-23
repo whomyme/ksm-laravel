e3d61f63defcbd170268c6235d27c51ac9a7816b
Rasyidi - ROG <rasyidialwee@gmail.com>
2022-03-23 15:30:49 +0800

    complete show

---
+++ resources/views/staff/show.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Sistem Staff</title>
+</head>
+
+<body>
+    <div class="container mt-5">
+        <h1>Staff {{ $staff->name }}</h1>
+        <div class="card">
+            <div class="card-body">
+                <h4>{{ $staff->name }}</h4>
+                <p><strong>{{ $staff->email }}</strong></p>
+                <p>{{ $staff->phone }}</p>
+                <p>{{ $staff->address }}</p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
